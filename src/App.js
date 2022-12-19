65a49b51719ab3509932c6f3bc8f7fca16b694e5
unknown <seshutholeti@gmail.com>
2022-12-19 14:57:58 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+
+  const[data,setData] = useState(['']);
+  useEffect(() =>{
+    fetch('https://data.covid19india.org/data.json').then(
+      res => res.json()
+    ).then(jsondata => setData(jsondata.statewise))
+  },[])
+
+
+
+
+
+  return(
+  <div>
+    <center>
+  <h1>India Covid19 Tracker APP</h1>
+         <table className='table'>
+          <thead className='thead-dark'>
+            <tr>
+              <th>State</th>
+              <th>Confirmed</th>
+              <th>Recovered</th>
+              <th>Deaths</th>
+              <th>Active</th>
+              <th>LastUpdate</th>
+            </tr>
+
+          </thead>
+          <tbody>
+           {data.map(item=>{
+           return(
+
+            <tr>
+              <td>{item.state}</td>
+              <td>{item.confirmed}</td>
+              <td>{item.recovered}</td>
+              <td>{item.deaths}</td>
+              <td>{item.active}</td>
+              <td>{item.lastupdatedtime}</td>
+            </tr>
+           )
+           })}
+           
+            
+
+
+          </tbody>
+
+
+         </table>
+  </center>
+</div>
+
+
+)
+
+
+
 }
 
 export default App;
